b92ccfc8d542bb725685a5d36ddc1e87c1ed775a
Rafael Maia <rafaelmaiarios@hotmail.com>
2022-06-16 17:21:14 -0300

    testando a logica do try catch

--- ExececoesJava/src/br/com/alura/app/Fluxo.java
+++ ExececoesJava/src/br/com/alura/app/Fluxo.java
@@ -10,17 +10,17 @@ public class Fluxo {
     * */
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
-        System.out.println("Fim do main");
+        try{
+            metodo1();
+        }catch (ArithmeticException e){
+            System.out.println(e);
+        }
+            System.out.println("Fim do main");
     }
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
-     try{
             metodo2();
-        }catch (ArithmeticException e){
-            System.out.println(e);
-        }
         System.out.println("Fim do metodo1");
     }
 
