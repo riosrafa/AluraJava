cc2313408e8ee3f66ad772c99b8e45b5ff545627
Rafael Maia <rafaelmaiarios@hotmail.com>
2022-06-16 17:12:15 -0300

    modificando o try catch

--- ExececoesJava/src/br/com/alura/app/Fluxo.java
+++ ExececoesJava/src/br/com/alura/app/Fluxo.java
@@ -16,7 +16,11 @@ public class Fluxo {
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
-        metodo2();
+     try{
+            metodo2();
+        }catch (ArithmeticException e){
+            System.out.println(e);
+        }
         System.out.println("Fim do metodo1");
     }
 
@@ -24,11 +28,7 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
-          try{
                 int a = i /0;
-            }catch (ArithmeticException e){
-              System.out.println(e);
-          }
         }
         System.out.println("Fim do metodo2");
     }
